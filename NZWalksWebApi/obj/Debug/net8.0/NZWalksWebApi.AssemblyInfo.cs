820d580f49b21d572a295b651b405b63e61f1633
Nithushan <jansonnithusanofficial@gmail.com>
2025-01-08 23:59:33 +0530

    newdb

--- NZWalksWebApi/obj/Debug/net8.0/NZWalksWebApi.AssemblyInfo.cs
+++ NZWalksWebApi/obj/Debug/net8.0/NZWalksWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalksWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00afe48009228cd2e685c1518872ae663b7eb1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7344b62e52d25a8fa773edd8acd828b441076ffa")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalksWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalksWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
